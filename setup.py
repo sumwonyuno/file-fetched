ef378fe5ade2572b8ab142c7713b7e86b6e72dc1
Mark <mmmunar@hawaii.edu>
2017-01-15 13:47:46 -0800

    adds setuptools setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='file-fetched',
+    version='0.1.0',
+    license='MIT',
+    url='https://github.com/sumwonyuno/file-fetched',
+    packages=[
+        'fetcher'
+    ],
+    install_requires=[
+        'certifi'
+    ]
+)
