9d0a7f872cf2aec183e239a1814262644cc82383
Mark <mmmunar@hawaii.edu>
2017-01-15 11:52:33 -0800

    adds http download test
    assumes content on the specific pastebin URLs are trusted

--- test/test_fetcher_fetch.py
+++ test/test_fetcher_fetch.py
@@ -17,7 +17,7 @@ class TestFetcherFetch(unittest.TestCase):
             # should go here
             pass
 
-    def test_run(self):
+    def test_run_local(self):
         tmp_dir = tempfile.mkdtemp()
         try:
             fetch.run(list_url='file:resources/sample.json', save_dir=tmp_dir)
@@ -25,5 +25,38 @@ class TestFetcherFetch(unittest.TestCase):
             self.assertTrue(os.path.exists(expected_path))
             with open(expected_path) as f:
                 self.assertEqual('content', f.read())
+        finally:
+            shutil.rmtree(tmp_dir)
+
+    def test_run_https(self):
+        tmp_dir = tempfile.mkdtemp()
+        url = 'https://raw.githubusercontent.com/sumwonyuno/file-fetched/dev/test/resources/sample_github.json'
+        try:
+            fetch.run(list_url=url, save_dir=tmp_dir)
+            expected_path = os.path.join(tmp_dir, 'from_github.txt')
+            self.assertTrue(os.path.exists(expected_path))
+            with open(expected_path) as f:
+                self.assertEqual('content', f.read())
+        finally:
+            shutil.rmtree(tmp_dir)
+
+    def test_run_http(self):
+        tmp_dir = tempfile.mkdtemp()
+        url = 'http://pastebin.com/raw/EbQj4FZF'
+        # the contents should be the following text:
+        # [
+        #   {
+        #     "name": "from_pastebin.txt",
+        #     "url": "http://pastebin.com/raw/SNTseZLu"
+        #   }
+        # ]
+        # the contents of SNTseZLu should be the following:
+        expected_content = "this text is on pastebin"
+        try:
+            fetch.run(list_url=url, save_dir=tmp_dir)
+            expected_path = os.path.join(tmp_dir, 'from_pastebin.txt')
+            self.assertTrue(os.path.exists(expected_path))
+            with open(expected_path) as f:
+                self.assertEqual(expected_content, f.read())
         finally:
             shutil.rmtree(tmp_dir)
