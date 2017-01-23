b1b5cd710d0ec12173bf65c32200df699b1ef198
Mark <mmmunar@hawaii.edu>
2017-01-22 14:51:29 -1000

    google drive download improvements

--- file_fetched/fetch.py
+++ file_fetched/fetch.py
@@ -4,6 +4,7 @@ import os
 
 import sys
 import urllib.request
+from html.parser import HTMLParser
 from urllib.error import URLError
 
 import certifi
@@ -50,6 +51,105 @@ def _hash_file(entry: dict, file_path: str) -> bool:
     return True
 
 
+def _handle_url(url: str, output_file: str):
+    chunk_size = 4096
+    # need to handle drive.google.com differently
+    if url.startswith('https://drive.google.com/'):
+        _handle_g_drive_url(url=url, output_file=output_file, chunk_size=chunk_size)
+    else:
+        _handle_default_url(url=url, output_file=output_file, chunk_size=chunk_size)
+
+
+def _handle_default_url(url: str, output_file: str, chunk_size: int):
+    with urllib.request.urlopen(url, cafile=certifi.where()) as f:
+        with open(output_file, 'wb') as output:
+            while True:
+                chunk = f.read(chunk_size)
+                if not chunk:
+                    break
+                output.write(chunk)
+
+
+class GDriveParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.direct_path = None
+
+    def handle_starttag(self, tag, attrs):
+        # need to get <a id="uc-download-link" href=<direct_path>...>
+        matched = False
+        for attr in attrs:
+            if attr[0] == 'id' and attr[1] == 'uc-download-link':
+                matched = True
+        if matched:
+            for attr in attrs:
+                if attr[0] == 'href':
+                    self.direct_path = attr[1]
+
+
+def _translate_g_drive_url(url: str) -> str:
+    # need to have in URL https://drive.google.com/uc?export=download&id=<id>
+    url_open = 'https://drive.google.com/open?id='
+    url_uc = 'https://drive.google.com/uc?export=download&id='
+    url_file_d = 'https://drive.google.com/file/d/'
+    # translate /open to /uc for direct file download
+    if url.startswith(url_open):
+        url = url.replace(url_open, url_uc)
+        print('Translated URL to %s' % url)
+    # translate /file/d/ to /uc for direct file download
+    if url.startswith(url_file_d):
+        url = url.replace(url_file_d, url_uc)
+        # may need to remove /view at the end of the URL
+        suffix_view = '/view'
+        if url.endswith(suffix_view):
+            url = url.replace(suffix_view, '')
+        print('Translated URL to %s' % url)
+    return url
+
+
+def _handle_g_drive_url(url: str, output_file: str, chunk_size: int):
+    url = _translate_g_drive_url(url)
+    with urllib.request.urlopen(url=url, cafile=certifi.where()) as f:
+        # expecting redirects
+        # if URL domain is not drive.google.com, just download URL contents
+        if not f.geturl().startswith('https://drive.google.com'):
+            # probably redirected to googleusercontent.com
+            _handle_default_url(url=url, output_file=output_file, chunk_size=chunk_size)
+            return
+        # if URL domain is still drive.google.com, then not actually downloading file
+        # may need to handle large files (< 25MB), will "Download anyway" page
+        # need to get real download link
+        print('Retrieving direct file download link; selecting \'Download anyway\' option')
+        # page should only be a few KB
+        parser = GDriveParser()
+        while True:
+            chunk = f.read(chunk_size)
+            if not chunk:
+                break
+            parser.feed(str(chunk))
+        if not parser.direct_path:
+            # workflow probably changed
+            print('Unable to retrieve updated direct file download link %s. skipping.' % url,
+                  file=sys.stderr)
+            return
+        new_url = 'https://drive.google.com' + parser.direct_path
+        # need to copy cookies
+        cookies = ''
+        for header, value in f.info().items():
+            if header == 'Set-Cookie':
+                cookies += value
+        headers = {'Cookie': cookies}
+        request = urllib.request.Request(url=new_url, headers=headers)
+        # fetch direct download URL contents
+        with urllib.request.urlopen(request) as f2:
+            with open(output_file, 'wb') as output:
+                while True:
+                    chunk = f2.read(chunk_size)
+                    if not chunk:
+                        break
+                    output.write(chunk)
+
+
 def run(list_url: str, save_dir: str):
     save_dir = os.path.abspath(save_dir)
     # make sure save_dir exists
@@ -61,7 +161,7 @@ def run(list_url: str, save_dir: str):
         sys.exit(1)
     print('Saving files to directory %s' % save_dir)
     # get list of files to download
-    print('Retriving list of files to download from URL %s' % list_url)
+    print('Retrieving list of files to download from URL %s' % list_url)
     try:
         with urllib.request.urlopen(list_url, cafile=certifi.where()) as f:
             json_data = json.loads(f.read().decode('UTF-8'))
@@ -114,9 +214,7 @@ def run(list_url: str, save_dir: str):
         try:
             # save tile to .partial file first before saving as the expected file
             partial_file = save_path + '.partial'
-            with urllib.request.urlopen(entry['url'], cafile=certifi.where()) as f:
-                with open(partial_file, 'wb') as output:
-                    output.write(f.read())
+            _handle_url(url=entry['url'], output_file=partial_file)
             # check .partial file matches any hashes
             if _hash_file(entry, partial_file):
                 print('Downloaded file, all hashes match.')
